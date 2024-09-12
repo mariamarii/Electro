86f28d220f051bcb01e009bfe5b5c3ebd13923af
maryam <mm5252@fayoum.edu.eg>
2024-09-12 20:50:09 +0300

    updates

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import '../style/cart.css'; // Import the CSS for styling
+import '../style/cart.css'; 
 import { removeFromCart } from '../store/cartActions';
 import { Link } from 'react-router-dom';
 
--- src/store/authActions.jsx
+++ src/store/authActions.jsx
@@ -1,28 +1,24 @@
 export const register = (userData) => (dispatch) => {
     return new Promise((resolve, reject) => {
       try {
-        // Get the list of users from localStorage
-        const users = JSON.parse(localStorage.getItem('users') || '[]'); // Default to an empty array if null
+        const users = JSON.parse(localStorage.getItem('users') || '[]'); 
   
-        // Check if the username is already taken
         const userExists = users.find((user) => user.username === userData.username);
         if (userExists) {
           alert('Username already exists. Please choose another.');
-          reject(); // Reject the promise if the username is taken
+          reject(); 
           return;
         }
   
-        // Add the new user to the users array
         users.push(userData);
         localStorage.setItem('users', JSON.stringify(users));
   
-        // Dispatch register action
         dispatch({ type: 'REGISTER', payload: userData });
   
-        resolve(); // Resolve the promise on successful registration
+        resolve(); 
       } catch (error) {
         console.error('Registration error:', error);
-        reject(); // Reject the promise on error
+        reject(); 
       }
     });
   };
@@ -35,26 +31,25 @@ export const register = (userData) => (dispatch) => {
       if (storedUser && storedUser.password === userData.password) {
         localStorage.setItem('user', JSON.stringify(storedUser));
         dispatch({ type: 'LOGIN', payload: storedUser });
-        resolve(); // Resolve the promise on successful login
+        resolve(); 
       } else if (storedUser) {
         alert('Invalid password');
-        reject(); // Reject the promise on invalid password
+        reject(); 
       } else {
         alert('User not found');
-        reject(); // Reject the promise on user not found
+        reject();
       }
     });
   };
-  // authActions.js
 export const logout = () => (dispatch) => {
     try {
-      // Clear user data from localStorage
       localStorage.removeItem('user');
   
-      // Dispatch logout action
       dispatch({ type: 'LOGOUT' });
     } catch (error) {
       console.error('Logout error:', error);
     }
   };
+
+  
   
